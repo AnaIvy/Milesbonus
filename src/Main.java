c653e81c0fb77b951709db441dc7ccb4351626d9
Papillon <anil_061293@gmail.com>
2023-01-28 23:36:15 +0300

    First commit

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        int amount = 15678;
+        int rubles = 20;
+        int bonus;
+        bonus = amount / rubles;
+        System.out.println("Количество начисленных миль за купленный билет = " + bonus);
+    }
+}
